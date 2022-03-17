8c4be8b04562f478266675e3a4b5c519d4a6b2f5
Cat <2107502565@qq.com>
2022-03-17 15:33:31 +0800

    git reflog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyC02Component } from './myc02.component';
+import { MyC03Component } from './myc03/myc03.component';
+import { MyComponent01 } from './mycom01';
+import { Myc04Component } from './myc04/myc04.component';
+import { MyC05Component } from './my-c05/my-c05.component';
+import { Myc05Component } from './myc05/myc05.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //宣言/申明/宣称
+    AppComponent,
+    MyComponent01,
+    MyC02Component,
+    MyC03Component,
+    Myc04Component,
+    MyC05Component,
+    Myc05Component,
   ],
   imports: [
     BrowserModule,
--- src/app/mycom01.ts
+++ src/app/mycom01.ts
@@ -5,9 +5,12 @@ import { Component } from "@angular/core";
 
 //装饰器--用于指定calss的用途 本质：函数
 @Component({
-    template: '<h2>我的组件01</h2>',
-    selector: '.mycom01'
+    template: '<h2>我的组件01</h2><hr>',
+    selector: 'myc01'//当作什么来用 .XXX = class  [XXX] = 属性  XXX = 元素
 })
 export class MyComponent01 {
 
 }
+//1、创建组件
+//2、在某个模块中注册组件class
+//3、使用已经注册的组件
